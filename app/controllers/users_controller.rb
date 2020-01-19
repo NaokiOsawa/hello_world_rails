ac44f774e173c39bb0ebc4e2226281f7b690f22e
大沢直樹 <>
2020-01-19 22:32:35 +0900

    implement-users-crud-vol2

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,31 @@
 class UsersController < ApplicationController
+  before_action:set_user,only: [:show,:update,:destroy]
+
   def index
     @users = User.all
   end
 
   def show
-    binding.pry
     @user = User.find(params[:id])
   end
+
+  def update
+    binding.pry
+    @user = User.find(params[:id])
+    @user.update!(user_params)
+  end
+
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy!
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:account, :name, :email)
+    end
+
+    def set_user
+      @user = User.find(params[:id])
+    end
 end
